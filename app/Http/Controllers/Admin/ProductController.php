3111a208c5e92cfa199709c23525829c96c19d18
anggarasa <anggasaputra6259@gmail.com>
2025-12-22 15:18:37 +0700

    refactor: fix pagination

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -7,14 +7,19 @@ use App\Http\Requests\Admin\Product\StoreProductRequest;
 use App\Http\Requests\Admin\Product\UpdateProductRequest;
 use App\Models\Category;
 use App\Models\Product;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::with('category', 'media')->latest()->paginate(10);
+        $limit = $request->input('limit', 10);
+        $products = Product::with('category', 'media')
+            ->latest()
+            ->paginate($limit)
+            ->withQueryString();
 
         return Inertia::render('admin/products/index', [
             'products' => $products,
